82ac5ea64283fd7464bf9e95f1eb0d7dcdd557ef
leleliu008 <leleliu008@gmail.com>
2022-05-11 15:44:06 +0800

    optimized

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ mkdir bin
 for item in linux/x86_64/*.tar.xz
 do
     tar vxf "$item"
-    cp -r   "linux/x86_64/$(basename "$item" .tar.xz)/bin" bin
+    cp -r   $(basename "$item" .tar.xz)/bin bin
 done
 
 for item in bin/*
